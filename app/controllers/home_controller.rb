73229fda58032bc1bb126814e1f72331e28b3ae4
leticia <tishcode@gmail.com>
2022-06-13 17:19:22 -0300

    add report route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def report
+    orders = Orders.where("created_at > ?", 30.days.ago)
+
+    send_data(orders.to_csv, filename: "orders.csv")
+  end
 end
