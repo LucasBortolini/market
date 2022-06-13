69b22d0c593b666424fc3157de227a85b5d92c2a
Lucas Bortolini <saleslucas99@gmail.com>
2022-06-13 15:12:44 -0300

    routes to admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'admin', to: 'admin#index'
+  get '/products/:id', to: 'products#view', as: :product_view
 
   scope :admin do
     resources :branches
